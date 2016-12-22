cbd3c8bcfcbfeb94af33e7d847b4e31b26c38f1f
Tang Bo Hao <btspoony@gmail.com>
2016-12-22 16:55:59 +0800

    move wechat check to activity controller

--- application/controllers/Activity.php
+++ application/controllers/Activity.php
@@ -10,6 +10,18 @@ class Activity extends TOP_Controller{
     
     function __construct() {
         parent::__construct();
+        
+        //危险严重
+        $this->wx_wechat();
+    }
+    
+    /**
+     * 微信验证
+     */
+    public function wx_wechat(){
+        $this->load->library('lib_wechat');
+        $data = $this->lib_wechat->get_open_id(APPID,APPSECRET);
+        p($data,1);
     }
     
     /**
