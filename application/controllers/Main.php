8ce6ae072544d22ec2ff0469ec3df60e32000635
Tang Bo Hao <btspoony@gmail.com>
2016-12-22 22:44:21 +0800

    add editor view

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -6,6 +6,11 @@ class Main extends TOP_Controller{
     function __construct() {
         parent::__construct();
     }
+
+    function h5editor(){
+        $type = 1;
+        $this->load->view('h5editor/index', compact('type'));
+    }
     
     /**
      * 路由判断跳转
