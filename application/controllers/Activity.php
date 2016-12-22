a62257d02b1284d4621d1658ecefd6ce504d3dd0
Tang Bo Hao <btspoony@gmail.com>
2016-12-22 16:56:45 +0800

    tiny

--- application/controllers/Activity.php
+++ application/controllers/Activity.php
@@ -16,15 +16,6 @@ class Activity extends TOP_Controller{
     }
     
     /**
-     * 微信验证
-     */
-    public function wx_wechat(){
-        $this->load->library('lib_wechat');
-        $data = $this->lib_wechat->get_open_id(APPID,APPSECRET);
-        p($data,1);
-    }
-    
-    /**
      * 首页
      * @param type $viewname
      */
