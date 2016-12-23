fba70b715402d605598eb1025fc81c715b4e230d
Tang Bo Hao <btspoony@gmail.com>
2016-12-23 17:01:17 +0800

    add mycodes page

---
+++ application/views/m/activity/mycodes.php
@@ -0,0 +1,20 @@
+<?php load_view('elements/header'); ?>
+<style>
+</style>
+
+<body>
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="javascript:void(0);">
+        <img alt="Brand" src="<?= image_path('m/xmas/logo.png'); ?>">
+      </a>
+    </div>
+  </div>
+</nav>
+
+<div>
+  
+</div>
+
+<?php load_view('elements/footer'); ?>
